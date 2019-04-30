be7f4bf3827d9498b28affd6c86cbc404360f177
Benco <covillebenoit@gmail.com>
2019-04-30 13:52:22 +0100

    Update database.sql
    modification du moteur de table pour pouvoir gérer les clés étrangères

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `piscine_test`.`utilisateur` ( 
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE `piscine_test`.`utilisateur` (
 	`utilisateur_id` INT NOT NULL AUTO_INCREMENT,
 	`utilisateur_nom` VARCHAR(255) NOT NULL ,
 	`utilisateur_prenom` VARCHAR(255) NOT NULL ,
@@ -8,30 +9,30 @@ CREATE TABLE `piscine_test`.`utilisateur` (
 	`utilisateur_photo` VARCHAR(255) NULL ,
 	`utilisateur_type` VARCHAR(8) NOT NULL ,
 	`utilisateur_vendeur_photo` VARCHAR(255) NULL ,
-	`utilisateur_acheteur_adresse` VARCHAR(255) NULL , 
-	`utilisateur_acheteur_cb` INT NULL , 
+	`utilisateur_acheteur_adresse` VARCHAR(255) NULL ,
+	`utilisateur_acheteur_cb` INT NULL ,
 	PRIMARY KEY (`utilisateur_id`)
-) ENGINE = MyISAM;
+) ENGINE = INNODB;
 
-
+DROP TABLE IF EXISTS `item`;
 CREATE TABLE `piscine_test`.`item` (
-	`item_id` INT NOT NULL AUTO_INCREMENT , 
-	`item_titre` VARCHAR(255) NOT NULL , 
-	`item_prix` INT NOT NULL , 
-	`item_description` TEXT NULL , 
-	`item_photo` VARCHAR(255) NULL , 
-	`item_video` VARCHAR(255) NULL , 
-	`item_qte_stock` INT NOT NULL , 
-	`item_qte_vendue` INT NOT NULL , 
-	`item_type` VARCHAR(8) NOT NULL , 
-	`item_livre_auteur` VARCHAR(255) NULL , 
-	`item_livre_date_publication` DATE NULL , 
-	`item_musique_artiste` VARCHAR(255) NULL , 
-	`item_musique_style` VARCHAR(255) NULL , 
-	`item_date_sortie` DATE NULL , 
-	`item_vetement_sexe` VARCHAR(1) NULL , 
-	`item_vetement_couleur` VARCHAR(10) NULL , 
-	`item_vetement_taille` VARCHAR(3) NULL , 
-	`item_sport_categorie` VARCHAR(255) NULL , 
+	`item_id` INT NOT NULL AUTO_INCREMENT ,
+	`item_titre` VARCHAR(255) NOT NULL ,
+	`item_prix` INT NOT NULL ,
+	`item_description` TEXT NULL ,
+	`item_photo` VARCHAR(255) NULL ,
+	`item_video` VARCHAR(255) NULL ,
+	`item_qte_stock` INT NOT NULL ,
+	`item_qte_vendue` INT NOT NULL ,
+	`item_type` VARCHAR(8) NOT NULL ,
+	`item_livre_auteur` VARCHAR(255) NULL ,
+	`item_livre_date_publication` DATE NULL ,
+	`item_musique_artiste` VARCHAR(255) NULL ,
+	`item_musique_style` VARCHAR(255) NULL ,
+	`item_date_sortie` DATE NULL ,
+	`item_vetement_sexe` VARCHAR(1) NULL ,
+	`item_vetement_couleur` VARCHAR(10) NULL ,
+	`item_vetement_taille` VARCHAR(3) NULL ,
+	`item_sport_categorie` VARCHAR(255) NULL ,
 	PRIMARY KEY (`item_id`)
-) ENGINE = MyISAM;
+) ENGINE = INNODB;
