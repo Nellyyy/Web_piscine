3aee58ab3d3b979e1fc359c9f29ddfa0e5e62feb
Nelly <lyla1998@gmail.com>
2019-04-30 11:47:55 +0200

    Menu and footer
    !! les fichier .html doivent etre en .php
    copier ces lignes à l'endroit du menu (début) et du footer (fin) : 
    
    <?php include("menu.php"); ?>
    <?php include("footer.php"); ?>

---
+++ menu.php
@@ -0,0 +1,27 @@
+<!--menu-->
+	<!-- source : https://www.w3schools.com/bootstrap/bootstrap_navbar.asp-->
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid menu">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">Ece Amazon</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li class="dropdown">
+	        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Catégories
+	        <span class="caret"></span></a>
+	        <ul class="dropdown-menu">
+	          <li><a href="livres.html">Livres</a></li>
+	          <li><a href="vetements.html">Vêtements</a></li>
+	          <li><a href="musiques.html">Musique</a></li>
+	          <li><a href="sports.html">Sport et Loisir</a></li>
+	        </ul>
+	      </li>
+	      <li><a href="flash.html">Vente flash</a></li>
+	      <li><a href="vendre.html">Vendre</a></li>
+	     </ul>
+	  	 <ul class="nav navbar-nav navbar-right">
+	      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign In</a></li>
+	      <li><a href="vendre.html"><img src="img/panier.png" width="20px" height="20px"></li>
+	    </ul>
+	  </div>
+	</nav>
