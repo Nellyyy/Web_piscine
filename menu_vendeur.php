10df0d9d6fd87fa2d7eb54435e0f49a102e2ebe9
Benco <covillebenoit@gmail.com>
2019-05-01 12:11:21 +0100

    display vendeur info in menu_vendeur

--- menu_vendeur.php
+++ menu_vendeur.php
@@ -21,19 +21,38 @@
 	<h2>Vos informations</h2>
 	<div>
 		<?php
+			//Je crée une variable locale email pour simuler la connexion, on devra récupérer ça d'une autre page plus tard
+			$email = "covillebenoit@gmail.com";
+
 			//identifier votre BDD
 			$database = "piscine_test";
 			//connectez-vous dans votre BDD
 			//Rappel: votre serveur = localhost et votre login = root et votre password = <rien>
 			$db_handle = mysqli_connect('localhost', 'root', '');
 			$db_found = mysqli_select_db($db_handle, $database);
+
 			if ($db_found)
 			{
-				echo "Connecté à la base de donnée";
+				echo "Connecté à la base de donnée <br/>";
+				//$sql = "SELECT * FROM 'utilisateur' WHERE 'utilisateur_email' LIKE '$email'";
+				$sql = "SELECT * FROM `utilisateur` WHERE `utilisateur_email` LIKE 'covillebenoit@gmail.com'";
+				$result = mysqli_query($db_handle, $sql);
+				if($result != NULL)
+				{	
+					while ($data = mysqli_fetch_assoc($result)) 
+					{
+						echo "Nom:" . $data['utilisateur_nom'] . '<br>';
+						echo "Prénom: " . $data['utilisateur_prenom'] . '<br>';
+					}
+				}
+				else
+				{
+					echo "result est null <br/>";
+				}
 			}
 			else
 			{
-				echo "Database not found";
+				echo "Database not found <br/>";
 			}
 			//affiche le nom et le prenom du vendeur
 		?>
