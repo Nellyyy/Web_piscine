5d897d86dca34e0f2166569e383592a3e8dfe545
Benco <covillebenoit@gmail.com>
2019-05-01 11:39:32 +0100

    connexion BDD

--- menu_vendeur.php
+++ menu_vendeur.php
@@ -18,11 +18,32 @@
 </head>
 <body>
 	<h1>Menu vendeur</h1>
+	<h2>Vos informations</h2>
+	<div>
+		<?php
+			//identifier votre BDD
+			$database = "piscine_test";
+			//connectez-vous dans votre BDD
+			//Rappel: votre serveur = localhost et votre login = root et votre password = <rien>
+			$db_handle = mysqli_connect('localhost', 'root', '');
+			$db_found = mysqli_select_db($db_handle, $database);
+			if ($db_found)
+			{
+				echo "Connecté à la base de donnée";
+			}
+			else
+			{
+				echo "Database not found";
+			}
+			//affiche le nom et le prenom du vendeur
+		?>
+		<br/>
+	</div>
 	<h2>voulez vous changer votre fond d'écran ?</h2>
 	<p>Voici votre fond d'écran</p>
 	<!--ici on va chercher le nom de la photo dans la base de donnée et on affiche la photo stockée sur le serveur-->
 	<?php 
-	
+		
 	?>
 </body>
 </html>
