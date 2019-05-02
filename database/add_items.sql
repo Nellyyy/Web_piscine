21f033290b0bdebb2895af70a079d900eee2fba8
Nelly <lyla1998@gmail.com>
2019-05-02 16:12:17 +0200

    update add item BDD

---
+++ database/add_items.sql
@@ -0,0 +1,6 @@
+INSERT INTO `item`  VALUES
+(NULL, 'jupette',25,'juupe dete super legere','photo','video',40,0,'vetement', 'a ',1999,' m', 's ',2000,'f','rose','M','gym','lyla1998@gmail.com'),
+(NULL, 'robe',25,'juupe dete super legere','photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','rose','S',' ','lyla1998@gmail.com'),
+(NULL, 'jean',25,'jean skinny','photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','beige','L',' ','lyla1998@gmail.com'),
+(NULL, 'pull',25,'pull go gros','photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','bleu','XS',' ','lyla1998@gmail.com');
+
