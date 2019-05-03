f51cd6c2d240b0511b863549d44f214617c1f588
Benoit <covillebenoit@gmail.com>
2019-05-03 20:57:21 +0100

    Suppression des vendeurs ok

--- supprimer_vendeur.php
+++ supprimer_vendeur.php
@@ -6,8 +6,20 @@
 	$db_handle = mysqli_connect('localhost', 'root', '');
 	$db_found = mysqli_select_db($db_handle, $database);
 	
-	//supprimer l'adresse du mec
+	//supprimer le panier
+	$sql = "DELETE FROM `panier` WHERE `utilisateur_email` LIKE '$email_suppr'";
+	mysqli_query($db_handle, $sql);
+	//supprime les objets vendues par le mec
+	$sql = "DELETE FROM `item` WHERE `utilisateur_email` LIKE '$email_suppr'";
+	mysqli_query($db_handle, $sql);
+	//Livraison (il ne devrait pas y en avoir)
+	$sql = "DELETE FROM `livraison` WHERE `utilisateur_email` LIKE '$email_suppr'";
+	mysqli_query($db_handle, $sql);
+	//Paiement (il ne devrait pas y en avoir)
+	$sql = "DELETE FROM `paiement` WHERE `utilisateur_email` LIKE '$email_suppr'";
+	mysqli_query($db_handle, $sql);
 
+	//Supprimer l'utilisateur
 	$sql = "DELETE FROM `utilisateur` WHERE `utilisateur_email` LIKE '$email_suppr'";
 	echo $sql . "<br/>";
 	
@@ -16,4 +28,6 @@
 		echo "Faux";
 	else if($result==1)
 		echo 'True';
+
+	header("Location: gestion_vendeurs.php")
 ?>
