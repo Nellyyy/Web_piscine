3dae4a8440aa0cf8b32dd5dfc0eb3dcffe19d891
Benoit <covillebenoit@gmail.com>
2019-05-03 21:32:16 +0100

    ménage de printemps

--- database/add_items.sql
+++ database/add_items.sql
@@ -1,6 +1,6 @@
 INSERT INTO `item`  VALUES
-(NULL, 'jupette',25,'juupe dete super legere','photo','video',40,0,'vetement', 'a ',1999,' m', 's ',2000,'f','rose','M','gym','lyla1998@gmail.com'),
-(NULL, 'robe',25,'juupe dete super legere','photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','rose','S',' ','lyla1998@gmail.com'),
-(NULL, 'jean',25,'jean skinny','photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','beige','L',' ','lyla1998@gmail.com'),
-(NULL, 'pull',25,'pull go gros','photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','bleu','XS',' ','lyla1998@gmail.com');
+(NULL, 'jupette',25,'juupe dete',  'photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','rose', 'M', '','jeanbenoit@gmail.com'),
+(NULL, 'robe',   25,'juupe dete',  'photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','rose', 'S', '','jeanbenoit@gmail.com'),
+(NULL, 'jean',   25,'jean skinny', 'photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','beige','L', '','jeanbenoit@gmail.com'),
+(NULL, 'pull',   25,'pull go gros','photo','video',40,0,'vetement', ' ',1999,' ', ' ',2000,'f','bleu', 'S', '','jeanbenoit@gmail.com');
 
