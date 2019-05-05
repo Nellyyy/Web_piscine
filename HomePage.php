0aaa1025082c9d07d5c85b76da27d6af4e028f5d
Nelly <lyla1998@gmail.com>
2019-05-05 18:55:33 +0200

    styles part7 homepage

--- HomePage.php
+++ HomePage.php
@@ -32,16 +32,53 @@ session_start();
 
   <!--images et lien vers chaque catégorie--> 
 
-	<div class="container-fluid accueil">
+	<div class="container-fluid ">
      <div class="pub">
        <h3>A ne pas manquer!</h3>
        <h1>Livraison gratuite dès 50$ d'achats!*</h1>
        <h6>*offre valable le mois de Mai, prix soumis à condition</h6>
      </div> 
-        
-      
   </div>
 	
+  <div class="ole1" >
+  <div class="container-fluid wrap">
+    <div class="pub2">
+       <h3>Catégorie Livres</h3>
+       <h1>Des milliers de livres disponibles à dévorer!</h1>
+       <h6>Pour les petits, les grands, des mangas aux grands classiques français.</h6>
+     </div> 
+  </div>
+</div>
+
+<div class="ole4" >
+  <div class="container-fluid wrap">
+    <div class="pub2">
+       <h3>Catégorie Vêtement</h3>
+       <h1>Vous trouverez ici toutes les dernières nouveautés mode!</h1>
+       <h6>Il y en a pour tous les styles.</h6>
+     </div> 
+  </div>
+</div>
+
+<div class="ole3" >
+  <div class="container-fluid wrap">
+    <div class="pub2">
+       <h3>Catégorie Musique</h3>
+       <h1>Et s'il était possible d'aller voir Armstrong en concert?</h1>
+       <h6>Explorez la catégorie musique pour vous évader.</h6>
+     </div> 
+  </div>
+</div>
+
+<div class="ole2" >
+  <div class="container-fluid wrap">
+    <div class="pub2">
+       <h3>Catégorie Sport et Loisir</h3>
+       <h1>Le sport il n'y a que ça de vrai!</h1>
+       <h6>You can do it.</h6>
+     </div> 
+  </div>
+</div>
     <!--footer-->
 	<?php include("footer.php"); ?>
 	<!--footer-->
