a929c833e42a4988ee333607a4e79d6ec718d546
Benoit <covillebenoit@gmail.com>
2019-05-05 11:56:57 +0100

    accès aux pages vendre/panier que pour vendeurs/acheteurs

--- deconnexion.php
+++ deconnexion.php
@@ -3,6 +3,9 @@
 
 	//reset all session varables
 	$_SESSION["email"] = NULL;
+	$_SESSION["type"] = NULL;
+	$_SESSION["nom"] = NULL;
+	$_SESSION["prenom"] = NULL;
 	$_SESSION["try_connect"] = NULL;
 
 	//it's better to not close the php tag at the end of file containing only php
